2b429e6c3d4b83e7f2935fc728ccc963c0b5134c
madbe <madbe72@gmail.com>
2016-05-31 13:18:03 +0300

    Add files via upload

---
+++ File-upload-in-Node-master/Server.js
@@ -0,0 +1,83 @@
+var express	=	require("express"),
+    bodyParser = require('body-parser'),
+    path     = require('path'),
+    fs = require('fs'),
+    buf = new Buffer(1024),
+    multer	=	require('multer');
+
+var app	=	express();
+
+// // upload the file and store it on the disk.
+// var storage	=	multer.diskStorage({
+//   destination: function (req, file, callback) {
+//     callback(null, './uploads');
+//   },
+//   filename: function (req, file, callback) {
+    
+//     callback(null, "upload-date-" + Date.now() + '-' + file.originalname);
+//   }
+// });
+
+
+// // var upload = multer({ storage : storage}).single('file');
+// var upload = multer({ storage : storage}).any();
+// //-----------------------------------------------------
+// //upoad file and stores the files in memory as Buffer objects.
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage }).any();
+// console.log("Going to open file!");
+// fs.open(__dirname + '/uploads/input.txt', 'r+', function(err, fd) {
+//    if (err) {
+//        return console.error(err);
+//    }
+//   console.log("File opened successfully!"); 
+//   fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+//       if (err){
+//          console.log(err);
+//       }
+//       console.log(bytes + " bytes read");
+      
+//       // Print only read bytes to avoid junk.
+//       if(bytes > 0){
+//          console.log(buf.slice(0, bytes).toString());
+//       }
+//    });  
+// });
+
+app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',function(req,res){
+      res.render('/index');
+});
+
+app.post('/api/photo',function(req,res){
+	upload(req, res, function(err) {
+		if(err) {
+			return res.end("Error uploading file.");
+		}
+    console.log(req.files);
+    // parseFiles(req.files);
+    //console.log(req.buffer.toJSON());
+		res.end("File is uploaded" + req.files.buffer);
+	});
+});
+
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
+
+// function parseFiles (Files) {
+//   files.uploadedImages.forEach(function (element, index, array) {
+//       fs.readFile(element.path, filesPath);
+//       });    
+// }
+
+function filesPath (err, data) {
+  var newPath = __dirname + "/public/uploadsDirectoryname/" + element.name;
+  fs.writeFile(newPath, data, function (err) {
+    if(err) { console.log(err); }
+  });
+}
---
+++ File-upload-in-Node-master/public/js/uploadProgress.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    
+    var uploadForm =$('#uploadForm'); 
+    var bar = $('.bar');
+    var percent = $('.percent');
+    var status = $('#status');
+    var progress = $("progress");
+       
+    $(uploadForm).ajaxForm({
+        beforeSend: function() {
+            status.empty();
+            var percentVal = '0%';
+            // bar.width(percentVal)
+            percent.html(percentVal);
+        },
+        uploadProgress: function(event, position, total, percentComplete) {
+            var percentVal = percentComplete;
+            progress.val(percentVal);
+            // bar.width(percentVal + '%')
+            percent.html(percentVal + '%');
+            status.empty().text("File is uploading...");
+            console.log(percentVal, position, total);
+        },
+        success: function() {
+            var percentVal = '100';
+            progress.val(percentVal);
+
+            // bar.width(percentVal + '%')
+            percent.html(percentVal + '%');
+        },
+        complete: function(xhr) {
+            status.html(xhr.responseText);
+        }
+    });
+
+    var inputs = document.querySelectorAll( '.input-file' );
+    Array.prototype.forEach.call( inputs, function( input )
+    {
+        var label    = input.nextElementSibling,
+            labelVal = label.innerHTML;
+
+        input.addEventListener( 'change', function( e )
+        {
+            var fileName = '';
+            if( this.files && this.files.length > 1 )
+                fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+            else
+                fileName = e.target.value.split( '\\' ).pop();
+
+            if( fileName ) {
+                console.log(fileName);
+                label.querySelector( 'span' ).innerHTML = fileName;
+            }
+            else
+                label.innerHTML = labelVal;
+        });
+    });
+});
